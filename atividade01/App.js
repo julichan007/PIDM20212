95635e72be07d2eb7ff36f12912aa50a139cdd10
Juliana Barreto <julichan.sousa@gmail.com>
2021-10-07 14:29:34 -0300

    Atividade 1
    Atividade 1 - Nome, idade, cidade e curso.

---
+++ atividade01/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Nome: Juliana Barreto</Text>
+      <Text> Idade: 36 </Text>
+      <Text>Cidade: Quixada</Text>
+      <Text>Curso: Design Digital</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
